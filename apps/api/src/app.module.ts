32890aa0074fc51138401100a97a48f43776a7bc
Tolga <tolgaouzz@gmail.com>
2022-04-16 02:04:59 +0300

    Little changes

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -16,7 +15,6 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
     }),
     AuthModule,
   ],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_GUARD,
