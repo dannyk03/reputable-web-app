70daec890adbc8b34c5f04001812fac3ded237db
Tolga <tolgaouzz@gmail.com>
2022-05-11 18:43:55 +0300

    Build again...

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -18,7 +18,7 @@ module.exports = withTM({
     domains: ["drive.google.com"],
   },
   env: {
-    API_URL: process.env.API_URL || "http://localhost:4000/",
+    API_URL: process.env.API_URL || "http://localhost:4000",
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || "dev-mpvuqq8o.us.auth0.com",
     AUTH0_CLIENT_ID:
       process.env.AUTH0_CLIENT_ID || "4JpjfVVmuJzQOzxBjqQ4fzmmygy5VS6Z",
