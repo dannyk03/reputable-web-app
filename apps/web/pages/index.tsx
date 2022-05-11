9d354ffc6dfb59316d067b5fe7731792431a45ec
Tolga <tolgaouzz@gmail.com>
2022-05-11 19:39:22 +0300

    Update index.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,8 @@
 import CommunitiesListView from "../containers/Communities/List";
 import React from "react";
-import { useRouter } from "next/router";
 import { useCommunities } from "../_api/Communities/queries";
 
 export default function HomePage() {
-  const router = useRouter();
   const { data = [], isLoading, error, isFetching } = useCommunities();
   if (isLoading) {
     return <></>;
