68bcc7e522023168a22ce36daf66681e1abd5d1b
Danny K <danny.kaganovitch@gmail.com>
2022-06-28 11:47:53 -0400

    Improve the create new experiement blocker modal messages

--- apps/web/containers/Experiments/List.tsx
+++ apps/web/containers/Experiments/List.tsx
@@ -1,16 +1,16 @@
-import { AddIcon } from "@chakra-ui/icons";
-import { Flex, Box, VStack, HStack, Hide } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { PrimaryButton } from "../../components/Button";
-import { ExperimentCard } from "../../components/Experiments";
-import type { ICommunity, PopulatedExperiment } from "@reputable/types";
-import CommunityCard from "../../components/Communities/CommunityCard";
-import { useRouter } from "next/router";
-import SearchInput from "../../components/SearchInput";
-import { useApiContext } from "../../providers/ApiContext";
-import Modal from "../../components/Modal";
-import Fuse from "fuse.js";
-import { sortBy } from "lodash";
+import { AddIcon } from '@chakra-ui/icons';
+import { Flex, Box, VStack, HStack, Hide } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { PrimaryButton } from '../../components/Button';
+import { ExperimentCard } from '../../components/Experiments';
+import type { ICommunity, PopulatedExperiment } from '@reputable/types';
+import CommunityCard from '../../components/Communities/CommunityCard';
+import { useRouter } from 'next/router';
+import SearchInput from '../../components/SearchInput';
+import { useApiContext } from '../../providers/ApiContext';
+import Modal from '../../components/Modal';
+import Fuse from 'fuse.js';
+import { sortBy } from 'lodash';
 
 export interface ExperimentsListViewProps {
   experiments: PopulatedExperiment[];
@@ -22,39 +22,57 @@ export default function ExperimentsListView({
   community,
 }: React.PropsWithChildren<ExperimentsListViewProps>) {
   const router = useRouter();
-  const [searchInput, setSearchInput] = React.useState("");
+  const [searchInput, setSearchInput] = React.useState('');
   const [items, setItems] = React.useState(experiments || []);
 
-  const { user } = useApiContext();
+  const { user, authorized, isAdmin } = useApiContext();
 
   useEffect(() => {
     const options = {
       includeScore: true,
       // Search in `author` and in `tags` array
-      keys: ["title"],
+      keys: ['title'],
     };
 
     const fuse = new Fuse(experiments, options);
-    if (searchInput.trim() !== "") {
+    if (searchInput.trim() !== '') {
       const result = fuse.search(searchInput);
       setItems(
-        sortBy(result, ["score"])
+        sortBy(result, ['score'])
           .reverse()
-          .map((r) => r.item)
+          .map((r) => r.item),
       );
     } else {
       setItems(experiments);
     }
   }, [searchInput, experiments]);
 
+  let createNewExperiementModalTitle = 'Information';
+  if (!user) createNewExperiementModalTitle = 'Oops';
+
+  let createNewExperiementModalBody =
+    'You have to be approved by an administrator to be able to create an experiment';
+  if (!user)
+    createNewExperiementModalBody = 'Please sign up to create an experiment';
+  let createNewExperiementModalButtonName = 'Ok';
+
+  console.log(isAdmin);
   return (
-    <Flex direction="row" justify={["center", "space-between"]} w="100%">
+    <Flex direction="row" justify={['center', 'space-between']} w="100%">
       <Hide below="md">
-        <VStack w={["100%", "260px"]} gap={5}>
+        <VStack w={['100%', '260px']} gap={5}>
           <CommunityCard community={community} />
-          {!(user?.app_metadata?.role === "admin") ? (
+
+          {isAdmin ? (
+            <PrimaryButton
+              onClick={() => router.push(`/${community.slug}/create`)}
+              text="Create new experiment"
+              leftIcon={<AddIcon width="12px" height="12px" />}
+            />
+          ) : (
             <Modal
-              title="Information"
+              title={createNewExperiementModalTitle}
+              closeButtonTitle={createNewExperiementModalButtonName}
               button={
                 <PrimaryButton
                   text="Create new experiment"
@@ -62,15 +80,8 @@ export default function ExperimentsListView({
                 />
               }
             >
-              You have to be approved by an administrator to be able to create
-              an experiment
+              {createNewExperiementModalBody}
             </Modal>
-          ) : (
-            <PrimaryButton
-              onClick={() => router.push(`/${community.slug}/create`)}
-              text="Create new experiment"
-              leftIcon={<AddIcon width="12px" height="12px" />}
-            />
           )}
         </VStack>
       </Hide>
--- apps/web/providers/ApiContext.tsx
+++ apps/web/providers/ApiContext.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { gql, GraphQLClient } from "graphql-request";
-import { useAuth0 } from "@auth0/auth0-react";
-import type { IUser } from "@reputable/types";
+import React from 'react';
+import { gql, GraphQLClient } from 'graphql-request';
+import { useAuth0 } from '@auth0/auth0-react';
+import type { IUser } from '@reputable/types';
 
-const API_URL = process.env.API_URL.endsWith("/")
+const API_URL = process.env.API_URL.endsWith('/')
   ? process.env.API_URL.slice(0, -1)
   : process.env.API_URL;
 
@@ -38,6 +38,8 @@ interface IAPIContext {
   user?: IUser;
   refreshUser?: () => void;
   isLoading?: boolean;
+  isAdmin: boolean;
+  authorized: boolean;
 }
 
 const APIContext = React.createContext<IAPIContext>({});
@@ -55,15 +57,16 @@ export function APIContextProvider({ children }: React.PropsWithChildren<{}>) {
     isLoading: Auth0Loading,
   } = useAuth0();
   const [isLoading, setIsLoading] = React.useState(true);
-  const [user, setUser] = React.useState();
+  const [user, setUser] = React.useState<IUser | null>(null);
   const [r, setR] = React.useState(false);
   const refreshUser = () => setR((prevR) => !prevR);
+
   React.useEffect(() => {
     // isAuthenticated is retrieved from Auth0
     if (!Auth0Loading && isAuthenticated)
-      getAccessTokenSilently({ audience: "https://api.reputable.health" })
+      getAccessTokenSilently({ audience: 'https://api.reputable.health' })
         .then((token) => {
-          client.setHeader("Authorization", `Bearer ${token}`);
+          client.setHeader('Authorization', `Bearer ${token}`);
           client
             .request(meQuery)
             .then((r) => {
@@ -76,8 +79,13 @@ export function APIContextProvider({ children }: React.PropsWithChildren<{}>) {
       setIsLoading(false);
     }
   }, [isAuthenticated, getAccessTokenSilently, r]);
+
+  const isAdmin = user && user?.app_metadata?.role === 'admin';
+  const authorized = (user && user?.app_metadata?.isApproved) || false;
   return (
-    <APIContext.Provider value={{ user, client, refreshUser, isLoading }}>
+    <APIContext.Provider
+      value={{ user, client, refreshUser, isLoading, isAdmin, authorized }}
+    >
       {children}
     </APIContext.Provider>
   );
